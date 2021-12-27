561ade0dd1effd2c1b065ea03bee3cf8050ff559
Sean <seanhoyeonjin@gmail.com>
2021-12-26 19:38:58 -0500

    started the front page

---
+++ frontend/src/components/Nav.js
@@ -0,0 +1,17 @@
+import "./Nav.css";
+
+function Nav() {
+  return (
+    <div className="nav-container">
+      <div className="nav-logo">
+        <div>LOGO</div>
+      </div>
+      <div className="nav-option">
+        <div>Home</div>
+        <div>Others</div>
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
